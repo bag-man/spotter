16acc1c959448479a5bb0357fa7ed916a9893d93
Owen Garland <garland.owen@gmail.com>
2020-02-18 17:43:54 +0100

    feat: add basic pushshift query

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -1,16 +1,30 @@
-import { credentials, subreddits } from './bootstrap'
+import { credentials, subreddits, PUSHSHIFT_COMMENTS_API, hateSubs, PushShiftComment } from './bootstrap'
+import axios from 'axios'
 import RedditApi from 'reddit-ts'
 
 const r = new RedditApi(credentials)
 
 const main = async () => {
-  const comments = (await Promise.all(
-    subreddits.map(subreddit => r.comments(subreddit))
-  )).flat().map(x =>
-    ({ subreddit: x.subreddit, author: x.author, url: x.url })
+  // const comments = (await Promise.all(
+  //   subreddits.map(subreddit => r.comments(subreddit))
+  // )).flat().map(x =>
+  //   ({ subreddit: x.subreddit, author: x.author, url: x.url })
+  // )
+
+  // console.log(comments)
+
+  const badComments = await axios.get(PUSHSHIFT_COMMENTS_API, {
+    params: {
+      subreddit: hateSubs.toString(),
+      size: 1000
+    }
+  });
+
+  const authors = badComments.data.data.map((x: PushShiftComment) =>
+    ({ author: x.author, subreddit: x.subreddit, permalink: x.permalink })
   )
 
-  console.log(comments)
+  console.log(authors)
 }
 
 main()
