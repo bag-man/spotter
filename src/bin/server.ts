69c3195d04772a509eb3f3f19c856b0f10c0b216
Owen Garland <garland.owen@gmail.com>
2020-03-01 01:40:03 +0100

    fix: comment out database for jenny

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -8,7 +8,7 @@ import * as logger from 'morgan'
 import * as compression from 'compression'
 import { getAuthor, Profile } from '../lib/author'
 import { authorToMarkdown } from '../lib/markdown'
-import { client, getLeaderboard } from '../lib/db'
+// import { client, getLeaderboard } from '../lib/db'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -18,14 +18,14 @@ app.use(compression())
 
 app.get('/favicon.ico', (_req, res) => res.status(204))
 
-app.get('/leaderboard', async (req, res, next): Promise<void> => {
-  try {
-    res.setHeader('content-type', 'application/json')
-    res.send(await getLeaderboard())
-  } catch (e) {
-    next(e)
-  }
-})
+// app.get('/leaderboard', async (req, res, next): Promise<void> => {
+//   try {
+//     res.setHeader('content-type', 'application/json')
+//     res.send(await getLeaderboard())
+//   } catch (e) {
+//     next(e)
+//   }
+// })
 
 app.get('/:author', async (req, res, next): Promise<void> => {
   try {
@@ -43,6 +43,6 @@ app.get('/:author', async (req, res, next): Promise<void> => {
 })
 
 app.listen(port, async () => {
-  await client.connect()
+  // await client.connect()
   console.log(`Listening on http://localhost:${port}`)
 })
