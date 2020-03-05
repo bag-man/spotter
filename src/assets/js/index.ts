711c857dfec8013abaf490f9316722d30504f32d
Owen Garland <garland.owen@gmail.com>
2020-03-05 22:17:11 +0100

    fix: clipboard, hopefully

--- src/assets/js/index.ts
+++ src/assets/js/index.ts
@@ -1,7 +1,13 @@
 const copy = () => {
   const md = document.getElementsByClassName('markdown').item(0)?.innerHTML
 
-  if (md) navigator.clipboard.writeText(md)
+  if (md) {
+    navigator.permissions.query({ name: 'clipboard-write' as PermissionName }).then(result => {
+      if (result.state == "granted" || result.state == "prompt") {
+        navigator.clipboard.writeText(md)
+      }
+    })
+  }
 }
 
 const copyBtn = document.getElementsByClassName('copy').item(0)
