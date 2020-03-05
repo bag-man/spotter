c6c657a6e127f5a83b9612efdba52c7a6853b4bb
Owen Garland <garland.owen@gmail.com>
2020-03-05 22:27:54 +0100

    fix: attempt 2

--- src/assets/js/index.ts
+++ src/assets/js/index.ts
@@ -5,6 +5,8 @@ const copy = () => {
     navigator.permissions.query({ name: 'clipboard-write' as PermissionName }).then(result => {
       if (result.state == "granted" || result.state == "prompt") {
         navigator.clipboard.writeText(md)
+      } else {
+        (navigator as any).permissions.request({name: 'clipboard-write' as PermissionName})
       }
     })
   }
